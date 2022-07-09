7bb184a3db0f58f4bb837a395d2d0eeaf94ecb33
PsyCo99 <vladmihalconstantin30@gmail.com>
2022-07-09 21:39:00 +0200

    aggiunta entity

--- Entities/Ecarello.php
+++ Entities/Ecarello.php
@@ -1,10 +1,20 @@
 <?php
 class Carello extends Eprodotti{
 	private $quantita;
+	private $_merci = array();
 
+	public function addMerci($idP,$marcaP,$quantitaP,$prezzoP,$immaginiP) {
+        array_push(
+			parent::set_id_utente($idP);
+			parent::set_marca($marcaP);
+			parent::set_quantita($quantitaP);
+			parent::set_prezzo($prezzoP);
+			parent::set_immagini($immaginiP);
+		);
+    }
     
-	public function __construct($quantitaC,$idP,$marcaP,$quantitaP,$prezzoP,$immaginiP){
-		
+	/*public function __construct($quantitaC,$idP,$marcaP,$quantitaP,$prezzoP,$immaginiP){
+
 		$this->$quantita=$quantitaC;
 
 		parent::set_id_utente($idP);
@@ -13,7 +23,7 @@ class Carello extends Eprodotti{
         parent::set_prezzo($prezzoP);
         parent::set_immagini($immaginiP);
 		
-	}
+	}*/
 	
     public function get_quantita(){
 		return $this->$quantita;
