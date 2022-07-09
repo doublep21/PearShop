d705bf93a2dcf143162a7eb8742f4052a69880bf
PsyCo99 <vladmihalconstantin30@gmail.com>
2022-07-09 16:31:15 +0200

    aggiunta entity

---
+++ Entities/Eamministratore.php
@@ -0,0 +1,26 @@
+<?php
+class Amministratore extends Eusers {
+	private $admin;
+    
+	public function __construct($adminA,$id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC){
+
+		$this->$admin=$adminA;	
+    
+    parent::set_id_utente($id_utenteC);
+    parent::set_nome($nomeC);
+    parent::set_cognome($cognomeC);
+    parent::set_email($emailC);
+    parent::set_password($passwordC);
+    
+	}
+
+    public function get_admin(){
+		return $this->$admin;
+	}
+    
+    //
+    public function set_admin($adminA){
+		$this->$admin=$adminA;
+	}
+
+}
--- Entities/Ericondizionato.php
+++ Entities/Ericondizionato.php
@@ -1,14 +1,23 @@
 <?php
-class Ricondizionato {
+class Ricondizionato extends Etelusato{
 	private $data_ricondizionamento;
     private $condizioni_ri;
 	private $prezzo_ri;
    
     
-	public function __construct($data_ricondizionamentoR,$condizioni_riR,$prezzo_riR){
+	public function __construct($data_ricondizionamentoR,$condizioni_riR,$prezzo_riR,$condizioniT,$data_aquistoT,$prezzo_usT,$imeiT,$cond_schermoT,$cond_batteriaT,$cond_usuraT,$prezzo_aqT){
 		$this->$data_ricondizionamento=$data_ricondizionamentoR;	
         $this->$condizioni_ri=$condizioni_riR;
 		$this->$prezzo_ri=$prezzo_riR;
+
+		parent::set_condizioni($condizioniT);
+		parent::set_data_aquisto($data_aquistoT);	
+        parent::set_prezzo_us($prezzo_usT);	
+        parent::set_imei($imeiT);	
+        parent::set_cond_schermo($cond_schermoT);
+		parent::set_cond_batteria($cond_batteriaT);
+        parent::set_cond_usura($cond_usuraT);
+		parent::set_prezzo_aq($prezzo_aqT);
         
 	}
 	
