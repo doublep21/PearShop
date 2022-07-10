69ed137b15991750188a0bb390cc5e3110b5089f
PsyCo99 <vladmihalconstantin30@gmail.com>
2022-07-10 17:09:37 +0200

    Modifica

--- Entities/Ecommenti.php
+++ Entities/Ecommenti.php
@@ -1,19 +1,52 @@
 <?php
-class Commenti{
+class Commenti extends Eusers implements JsonSerializable{
 	private $rating;
-    
-	public function __construct($ratingC,){
+	private $commento
+    private $img = array(
+		"id" => 0,
+		"nome" => "",
+		"dimensione" => "",
+		"tipo" => ""
+	);
+	private $user = array(
+		"id" => 0,
+		"nome" => "",
+		"cognome" => "",
+	);
+
+
+	public function __construct($ratingC,$commentoC,$imgC,$userC,$id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC,$ordiniC,$carelloC){
+		parent::__construct($id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC,$ordiniC,$carelloC); 
 		$this->$rating=$ratingC;	
+		$this->$$commento=$$commentoC;	
+		$this->$$img=$$imgC;	
+		$this->$$user=$$userC;
 	}
 	
     public function get_rating(){
 		return $this->$rating;
+	}
+	public function get_commento(){
+		return $this->$commento;
+	}
+	public function get_immagini(){
+		return $this->$immagini;
 	}
     
     //
     public function set_rating($ratingC){
 		$this->$rating=$ratingC;
 	}
-
+	
+	public function jsonSerialize() : array
+    {
+        $result = array(
+            "rating" => $this->rating,
+            "commento" => $this->commento,
+			"img" => $this->img,
+			"user" => $this->user,			
+        );
+        return $result;
+    }
 }
 ?>
