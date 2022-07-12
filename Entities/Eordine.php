2dcb810e8c25cc96c17936dce172789f89f45492
PsyCo99 <vladmihalconstantin30@gmail.com>
2022-07-12 05:43:49 +0200

    Modifica

--- Entities/Eordine.php
+++ Entities/Eordine.php
@@ -1,16 +1,15 @@
 <?php
-class Ordine {
+class Ordine extends Ecarello{
 	private $prezzo_tot;
     private $ind_spedizione;
     private $data_ora;
-	private $prodotto = array();
 	private $codice_promozionale;
 
-	public function __construct($prezzo_totO,$ind_spedizioneO,$data_oraO,$prodottoO,$codice_promozionaleO){
+	public function __construct($prezzo_totO,$ind_spedizioneO,$data_oraO,$codice_promozionaleO,$quantitaCarelloC){
+		parent::__construct($quantitaCarelloC); 
 		$this->$prezzo_tot=$prezzo_totO;	
         $this->$ind_spedizione=$ind_spedizioneO;	
         $this->$data_ora=$data_oraO;
-		$this->$prodotto=$prodottoO;
 		$this->$codice_promozionale=$codice_promozionaleO;		
 	}
 	
@@ -22,9 +21,6 @@ class Ordine {
 	}
     public function get_data_ora(){
 		return $this->$data_orat;
-	}
-    public function get_prodotto(){
-		return $this->$prodotto;
 	}
 	public function get_codice_promozionale(){
 		return $this->$codice_promozionale;
@@ -38,9 +34,6 @@ class Ordine {
 	}
     public function set_data_ora($data_oraO){
 		$this->$data_ora=$data_oraO;
-	}
-	public function set_prodotto($prodottoO){
-		$this->$prodotto=$prodottoO;
 	}
 	public function set_codice_promozionale($codice_promozionaleO){
 		$this->$codice_promozionale=$codice_promozionaleO;
