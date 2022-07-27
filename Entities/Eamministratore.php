b6442d15d576f057efa80fb24824e17fc28313f5
PsyCo99 <vladmihalconstantin30@gmail.com>
2022-07-27 22:43:37 +0200

    Riscrittura classe Eamministratore

--- Entities/Eamministratore.php
+++ Entities/Eamministratore.php
@@ -1,22 +1,22 @@
 <?php
-class Amministratore extends Eusers {
+
+class Amministratore extends Eusers{
 	private $admin;
     
-	public function __construct($adminA,$id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC){
-
-		$this->$admin=$adminA;	
+	public function __construct($adminA,$id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC){	
     	parent::__construct($id_utenteC,$nomeC,$cognomeC,$emailC,$passwordC);
-    
+    	$this->admin=$adminA;
 	}
 
+	//----------------GET----------------//
     public function get_admin(){
-		return $this->$admin;
+		return $this->admin;
 	}
     
-    //
+    //----------------SET----------------//
     public function set_admin($adminA){
-		$this->$admin=$adminA;
+		$this->admin=$adminA;
 	}
-
 }
+
 ?>
